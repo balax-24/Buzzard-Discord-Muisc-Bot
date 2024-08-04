e5b19e5b0fb759617733408496c752fc2e9cd24e
balax-24 <balaharish0202@gmail.com>
2024-08-05 00:31:37 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -23,13 +23,13 @@
 
 module.exports = {
   TOKEN: "YOUR BOT TOKEN",
-  ownerID: ["YOUR CLIENT ID", ""], 
+  ownerID: ["YOUR ID", ""], 
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
   musicardTheme:"themes16", //Goes from themes1 to themes19
-  activityName: "Music | Owner - Buzzard", // This is bot status Write Anything here 
-  activityType: "Playing",  // Available activity types : LISTENING , PLAYING
+  activityName: "/help | Owner - Buzzard", // This is bot status Write Anything here 
+  activityType: "WATCHING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://dsc.gg/jods",
   CheckmarkIcon: "https://cdn.discordapp.com/attachments/1230824451990622299/1230836684774576168/7762-verified-blue.gif",
   MusicIcon:"https://media.discordapp.net/attachments/1230824451990622299/1236664581364125787/music-play.gif",
